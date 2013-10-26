c3a84b09255f857be0b677bb9716ad49d9388bc6
Alex Gavrishev <alex.gavrishev@gmail.com>
2013-10-26 15:43:35 +0300

    add car wheels

--- HW2/Car.cpp
+++ HW2/Car.cpp
@@ -26,17 +26,41 @@ Car::~Car(void)
 
 void Car::draw3d() {
 	
+	glTranslated(pos.x,pos.y,pos.z);
 
 	// car
 	glPushMatrix();
-		glTranslated(pos.x,pos.y,pos.z);
 		glRotated(angle*180/PI,0,1,0);  // yaw
 		//glRotated(-plane_ang_speed*2000,1,0,0); // roll
 		//glRotated(pitch*180/PI,0,0,1);
-		draw();;
+		draw();
 	glPopMatrix();
 
-	drawWheel();
+	// Wheels
+	glPushMatrix();
+		glTranslated(2.5f,1.0f,-3.0f);
+		glRotated(90,0,1,0);
+		drawWheel();
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslated(2.5f,1.0f,3.0f);
+		glRotated(90,0,1,0);
+		drawWheel();
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslated(-2.5f,1.0f,-3.0f);
+		glRotated(90,0,1,0);
+		drawWheel();
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslated(-2.5f,1.0f,3.0f);
+		glRotated(90,0,1,0);
+		drawWheel();
+	glPopMatrix();
+	
 }
 
 void Car::draw2d() {
@@ -48,75 +72,75 @@ void Car::draw() {
 
 	//front
 	glBegin(GL_POLYGON);
-	glColor3d(1.0f,0.0f,0.0f);
-	glVertex3d(-4.5f, 1.6f, -3.0f);              // Top Left
-	glVertex3d(-4.5f, 1.6f, 3.0f);              // Top Right
-	glVertex3d(-4.5f, 0.6f, 3.0f);              // Bottom Right
-	glVertex3d(-4.5f, 0.6f, -3.0f);              // Bottom Left
+	glColor3d(0.6f,0.0f,0.0f);
+		glVertex3d(-4.5f, 1.6f, -3.0f);              // Top Left
+		glVertex3d(-4.5f, 1.6f, 3.0f);              // Top Right
+		glVertex3d(-4.5f, 0.6f, 3.0f);              // Bottom Right
+		glVertex3d(-4.5f, 0.6f, -3.0f);              // Bottom Left
 	glEnd();
 	
 	//back
 	glBegin(GL_POLYGON);
-	glColor3d(1.0f,0.0f,0.0f);
-	glVertex3d( 4.5f, 1.6f, -3.0f);              // Top Left
-	glVertex3d( 4.5f, 1.6f, 3.0f);              // Top Right
-	glVertex3d( 4.5f, 0.6f, 3.0f);              // Bottom Right
-	glVertex3d( 4.5f, 0.6f, -3.0f);              // Bottom Left
+	glColor3d(0.6f,0.0f,0.0f);
+		glVertex3d( 4.5f, 1.6f, -3.0f);              // Top Left
+		glVertex3d( 4.5f, 1.6f, 3.0f);              // Top Right
+		glVertex3d( 4.5f, 0.6f, 3.0f);              // Bottom Right
+		glVertex3d( 4.5f, 0.6f, -3.0f);              // Bottom Left
 	glEnd();
 	
 	//left
 	glBegin(GL_POLYGON);
 	glColor3d(1.0f,0.0f,0.0f);
-	glVertex3d(-4.5f, 1.6f, -3.0f);              // Top Left
-	glVertex3d( 4.5f, 1.6f, -3.0f);              // Top Right
-	glVertex3d( 4.5f, 0.6f, -3.0f);              // Bottom Right
-	glVertex3d(-4.5f, 0.6f, -3.0f);              // Bottom Left
+		glVertex3d(-4.5f, 1.6f, -3.0f);              // Top Left
+		glVertex3d( 4.5f, 1.6f, -3.0f);              // Top Right
+		glVertex3d( 4.5f, 0.6f, -3.0f);              // Bottom Right
+		glVertex3d(-4.5f, 0.6f, -3.0f);              // Bottom Left
 	glEnd();
 
 	//right
 	glBegin(GL_POLYGON);
 	glColor3d(1.0f,0.0f,0.0f);
-	glVertex3d(-4.5f, 1.6f, 3.0f);              // Top Left
-	glVertex3d( 4.5f, 1.6f, 3.0f);              // Top Right
-	glVertex3d( 4.5f, 0.6f, 3.0f);              // Bottom Right
-	glVertex3d(-4.5f, 0.6f, 3.0f);              // Bottom Left
+		glVertex3d(-4.5f, 1.6f, 3.0f);              // Top Left
+		glVertex3d( 4.5f, 1.6f, 3.0f);              // Top Right
+		glVertex3d( 4.5f, 0.6f, 3.0f);              // Bottom Right
+		glVertex3d(-4.5f, 0.6f, 3.0f);              // Bottom Left
 	glEnd();
 
 	//bottom
 	glBegin(GL_POLYGON);
 	glColor3d(0.0f,1.0f,0.0f);
-	glVertex3d(-4.5f, 0.6f, -3.0f);              // Top Left
-	glVertex3d( 4.5f, 0.6f, -3.0f);              // Top Right
-	glVertex3d( 4.5f, 0.6f, 3.0f);              // Bottom Right
-	glVertex3d(-4.5f, 0.6f, 3.0f);              // Bottom Left
+		glVertex3d(-4.5f, 0.6f, -3.0f);              // Top Left
+		glVertex3d( 4.5f, 0.6f, -3.0f);              // Top Right
+		glVertex3d( 4.5f, 0.6f, 3.0f);              // Bottom Right
+		glVertex3d(-4.5f, 0.6f, 3.0f);              // Bottom Left
 	glEnd();
 
 	//trunk
 	glBegin(GL_POLYGON);
 	glColor3d(1.0f,0,0);
-	glVertex3d(-4.5f, 1.6f, -3.0f);              // Top Left
-	glVertex3d(-2.0f, 1.6f, -3.0f);              // Top Right
-	glVertex3d(-2.0f, 1.6f, 3.0f);              // Bottom Right
-	glVertex3d(-4.5f, 1.6f, 3.0f);              // Bottom Left
+		glVertex3d(-4.5f, 1.6f, -3.0f);              // Top Left
+		glVertex3d(-2.0f, 1.6f, -3.0f);              // Top Right
+		glVertex3d(-2.0f, 1.6f, 3.0f);              // Bottom Right
+		glVertex3d(-4.5f, 1.6f, 3.0f);              // Bottom Left
 	glEnd();
 
 	
 	//motor
 	glBegin(GL_POLYGON);
 	glColor3d(1.0f,0,0);
-	glVertex3d( 2.0f, 1.6f, -3.0f);              // Top Left
-	glVertex3d( 4.5f, 1.6f, -3.0f);              // Top Right
-	glVertex3d( 4.5f, 1.6f, 3.0f);              // Bottom Right
-	glVertex3d( 2.0f, 1.6f, 3.0f);              // Bottom Left
+		glVertex3d( 2.0f, 1.6f, -3.0f);              // Top Left
+		glVertex3d( 4.5f, 1.6f, -3.0f);              // Top Right
+		glVertex3d( 4.5f, 1.6f, 3.0f);              // Bottom Right
+		glVertex3d( 2.0f, 1.6f, 3.0f);              // Bottom Left
 	glEnd();
 
 	//top
 	glBegin(GL_POLYGON);
 	glColor3d(1.0f,0.0f,0.0f);
-	glVertex3d(-1.5f,2.6f, -3.0f);              // Top Left
-	glVertex3d( 1.5f,2.6f, -3.0f);              // Top Right
-	glVertex3d( 1.5f,2.6f, 3.0f);              // Bottom Right
-	glVertex3d(-1.5f,2.6f, 3.0f);              // Bottom Left
+		glVertex3d(-1.5f,2.6f, -3.0f);              // Top Left
+		glVertex3d( 1.5f,2.6f, -3.0f);              // Top Right
+		glVertex3d( 1.5f,2.6f, 3.0f);              // Bottom Right
+		glVertex3d(-1.5f,2.6f, 3.0f);              // Bottom Left
 	glEnd();
 
 	// Transparent glasses
@@ -126,34 +150,34 @@ void Car::draw() {
 
 	//front glass
 	glBegin(GL_POLYGON);
-	glVertex3d( 1.5f, 2.6f, -3.0f);              // Top Left
-	glVertex3d( 1.5f, 2.6f, 3.0f);              // Top Right
-	glVertex3d( 2.0f, 1.6f, 3.0f);              // Bottom Right
-	glVertex3d( 2.0f, 1.6f, -3.0f);              // Bottom Left
+		glVertex3d( 1.5f, 2.6f, -3.0f);              // Top Left
+		glVertex3d( 1.5f, 2.6f, 3.0f);              // Top Right
+		glVertex3d( 2.0f, 1.6f, 3.0f);              // Bottom Right
+		glVertex3d( 2.0f, 1.6f, -3.0f);              // Bottom Left
 	glEnd();
 
 	//back glass
 	glBegin(GL_POLYGON);
-	glVertex3d( -1.5f, 2.6f, -3.0f);              // Top Left
-	glVertex3d( -1.5f, 2.6f, 3.0f);              // Top Right
-	glVertex3d( -2.0f, 1.6f, 3.0f);              // Bottom Right
-	glVertex3d( -2.0f, 1.6f, -3.0f);              // Bottom Left
+		glVertex3d( -1.5f, 2.6f, -3.0f);              // Top Left
+		glVertex3d( -1.5f, 2.6f, 3.0f);              // Top Right
+		glVertex3d( -2.0f, 1.6f, 3.0f);              // Bottom Right
+		glVertex3d( -2.0f, 1.6f, -3.0f);              // Bottom Left
 	glEnd();
 
 	//right glass
 	glBegin(GL_POLYGON);
-	glVertex3d( -1.5f, 2.6f, 3.0f);              // Top Left
-	glVertex3d(  1.5f, 2.6f, 3.0f);              // Top Right
-	glVertex3d( 2.0f, 1.6f,  3.0f);              // Bottom Right
-	glVertex3d( -2.0f, 1.6f, 3.0f);              // Bottom Left
+		glVertex3d( -1.5f, 2.6f, 3.0f);              // Top Left
+		glVertex3d(  1.5f, 2.6f, 3.0f);              // Top Right
+		glVertex3d( 2.0f, 1.6f,  3.0f);              // Bottom Right
+		glVertex3d( -2.0f, 1.6f, 3.0f);              // Bottom Left
 	glEnd();
 
 	//left glass
 	glBegin(GL_POLYGON);
-	glVertex3d( -1.5f, 2.6f, -3.0f);              // Top Left
-	glVertex3d(  1.5f, 2.6f, -3.0f);              // Top Right
-	glVertex3d( 2.0f, 1.6f,  -3.0f);              // Bottom Right
-	glVertex3d( -2.0f, 1.6f, -3.0f);              // Bottom Left
+		glVertex3d( -1.5f, 2.6f, -3.0f);              // Top Left
+		glVertex3d(  1.5f, 2.6f, -3.0f);              // Top Right
+		glVertex3d( 2.0f, 1.6f,  -3.0f);              // Bottom Right
+		glVertex3d( -2.0f, 1.6f, -3.0f);              // Bottom Left
 	glEnd();
 
 	glDisable(GL_BLEND);
@@ -165,24 +189,28 @@ void Car::drawWheel()
 {
 	double alpha,x,y;
 	glColor3d(0,0,0);
-	//glScaled(0,0.5,0.5);
+	glLineWidth(25.0f);
+	glScaled(0,0.5,0.5);
 	glBegin(GL_LINE_LOOP);
-	for(alpha = 0; alpha<2*PI;alpha+=PI/20)
+	for(alpha = 0; alpha<2*PI;alpha+=PI/1000)
 	{
 		x = 0.7*cos(alpha);
 		y = 0.7*sin(alpha);
-		glVertex3d(0,x,y);
+		glVertex3d(0,x-0.5f,y);
 	}
 	glEnd();
 
-	glColor3d(1,1,1);
+	//silver
+	glColor3d(0.75f,0.75f,0.75f);
 	glBegin(GL_LINES);
-	for(alpha = 0; alpha<2*PI;alpha+=PI/6)
+	for(alpha = 0; alpha<2*PI;alpha+=PI/5)
 	{
-		x = 0.7*cos(alpha);
-		y = 0.7*sin(alpha);
-		glVertex3d(0,x,y);
-		glVertex3d(0,0,0);
+		x = 0.68*cos(alpha);
+		y = 0.68*sin(alpha);
+		glVertex3d(0,x-0.5f,y);
+		glVertex3d(0,-0.5f,0);
 	}
 	glEnd();
+	glLineWidth(1.0f);
+
 }
